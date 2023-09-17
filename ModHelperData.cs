0d8da55f621a075df73ee1a4bb3f8f81597b4115
Weadeater12 <126626651+Weadeater12@users.noreply.github.com>
2023-09-17 14:03:09 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "ExtraTowers";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Add my towers idea to the game";
 
     public const string RepoOwner = "Weadeater12"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "ExtraTowers"; // TODO add your repo name here, also in the download url in README.md
